9522f287661c9c086fb506b1a637e9b17cc02cb4
ANDRES <2amc80548@gmail.com>
2025-11-22 18:58:08 -0400

    cambio en vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-         base: process.env.NODE_ENV === 'production'
-         ? '/inf513/grupo05cc/coop-agua/public/build/'
-         : '/',
+        //  base: process.env.NODE_ENV === 'production'
+        //  ? '/inf513/grupo05cc/coop-agua/public/build/'
+        //  : '/',
    
 
     plugins: [
